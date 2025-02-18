83f7f086e9782102f2f82dc0804faa18dff369bb
Diego <31476803+diegodev9@users.noreply.github.com>
2025-02-18 17:34:16 -0300

    Css adjustment (#4)
    * Add before-after as a caroousel
    
    * fix for carousel responsive
    
    * fix styles. added modal for product information
    
    * Add dialog to product
    
    ---------
    
    Co-authored-by: david <juda9323@gmail.com>

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,51 +1,26 @@
-<template>
-  <div class="content-carousel-products">
-    <Carousel
-      v-if="elements && elements.length > 0"
-      :key="carouselKey"
-      :value="elements"
-      :numVisible="numVisible"
-      :numScroll="numScroll"
-      :responsiveOptions="responsiveOptions"
-      :autoplayInterval="2500"
-      >
-      <template #item="slotProps">
-        <div class="border-1 surface-border border-round m-2 p-3">
-          <div class="mb-3">
-            <div class="relative mx-auto flex justify-content-center">
-              <Image :src="slotProps.data.image" alt="Image" width="150" preview />
-              <Tag value="Stock" class="absolute" style="left:5px; top: 5px" />
-            </div>
-          </div>
-          <div class="mb-3 font-medium">{{ slotProps.data.name }}</div>
-          <div class="flex justify-content-between align-items-center">
-            <div class="mt-0 font-semibold text-xl">$ 30.00</div>
-            <span>
-              <Button icon="pi pi-heart" severity="secondary" outlined />
-              <Button icon="pi pi-shopping-cart" class="ml-2" />
-            </span>
-          </div>
-        </div>
-      </template>
-    </Carousel>
-  </div>
-</template>
-
 <script setup>
-import { ref, nextTick, onMounted, watch } from 'vue';
+import { ref, nextTick, onMounted, watch, inject } from 'vue';
 import Carousel from "primevue/carousel";
 import Image from 'primevue/image';
 import Tag from "primevue/tag";
 import Button from "primevue/button";
+import ConfirmDialog from "primevue/confirmdialog";
+import { useConfirm } from 'primevue/useconfirm';
+import Dialog from 'primevue/dialog';
+import InputText from 'primevue/inputtext';
+
 
 const props = defineProps({
   elements: Array,
   scenaryId: Number,
 });
 
-const numVisible = ref(4);
+const numVisible = ref(3);
 const numScroll = ref(1);
 const carouselKey = ref(Date.now());
+const dialogRef = inject('dialogRef');
+const confirm = useConfirm();
+const isVisible = ref(false);
 
 onMounted(() => {
   nextTick(() => {
@@ -55,15 +30,66 @@ onMounted(() => {
 
 const responsiveOptions = ref([
   { breakpoint: "1400px", numVisible: 2, numScroll: 1 },
-  { breakpoint: "1199px", numVisible: 3, numScroll: 1 },
-  { breakpoint: "767px", numVisible: 2, numScroll: 1 },
-  { breakpoint: "575px", numVisible: 1, numScroll: 1 },
+  //{ breakpoint: "1199px", numVisible: 3, numScroll: 1 },
+  { breakpoint: "767px", numVisible: 1, numScroll: 1 },
+  //{ breakpoint: "575px", numVisible: 1, numScroll: 1 },
 ]);
+
+const showProductInfo = (productData) => {
+  console.log("Esta es la data: ", productData.name);
+  confirm.require({
+    message: `${productData.description}`,
+    header: `${productData.name}`,
+    onShow: () => {
+      isVisible.value = true;
+    },
+    onHide: () => {
+      isVisible.value = false;
+    },
+    footer: false
+  })
+}
+
+const closeDialog = () => {
+  dialogRef.value.close();
+}
+
 </script>
-<style>
-  .content-carousel-products {
-    background-color: blue;
-    width: 100%;
-    padding: 10px;
-  }
-</style>
+
+<template>
+  <div class="">
+    <Carousel v-if="elements && elements.length > 0" :key="carouselKey" :value="elements" :numVisible="numVisible"
+      :numScroll="numScroll" :responsiveOptions="responsiveOptions">
+      <template #item="slotProps">
+        <div class="border-1 border-round m-2">
+          <div class="relative mx-auto">
+            <Image :src="slotProps.data.image" alt="Image" class="slide-img" />
+            <Tag value="Stock" class="absolute" style="left:5px; top: 5px" />
+          </div>
+          <div class="flex flex-column justify-content-between p-4">
+            <div class="mb-3">
+              <p class="m-0 font-bold" @click="slotProps.data.visible = true" style="cursor: pointer">
+                {{ slotProps.data.name }}
+              </p>
+            </div>
+            <div class="flex flex-auto flex-wrap justify-content-between align-items-center">
+              <p class="m-0 font-semibold text-xl">$ 30.00</p>
+              <div>
+                <Button icon="pi pi-heart" severity="secondary" outlined />
+                <Button icon="pi pi-shopping-cart" class="ml-2" />
+                <Dialog v-model:visible="slotProps.data.visible" :header="slotProps.data.name"
+                  :style="{ width: '25rem' }">
+                  <div class="text-center mb-4">
+                    <Image :src="slotProps.data.image" class="mx-auto" alt="Image" width="350" />
+                  </div>
+                  <span class="text-surface-500 dark:text-surface-400 block mb-8">{{ slotProps.data.description
+                  }}</span>
+                </Dialog>
+              </div>
+            </div>
+          </div>
+        </div>
+      </template>
+    </Carousel>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,125 +1,52 @@
-<template>
-  <Carousel
-    :value="scenarios"
-    :key="mainCarouselKey"
-    :numVisible="1"
-    :numScroll="1"
-    >
-    <template #item="{ data }">
-      <div class="content">
-        <div class="grid">
-          <div class="col-6" v-for="parentPhoto of data.parentPhotos">
-            <div class="content-image flex justify-content-center align-items-center">
-              <Image :src="parentPhoto.image" alt="Image" width="350" preview />
-            </div>
-            <div class="text-center p-3 border-round-sm bg-primary font-bold">
-              {{ parentPhoto.details }}
-            </div>
-          </div>
-        </div>
-      </div>
-      <div>
-        <Products 
-          :elements="data.products"
-          :key="`products-${data.id}`"
-          :scenaryId="data.id"
-        />
-      </div>
-    </template>
-  </Carousel>
-</template>
-
 <script setup>
 import Products from "@/components/Products.vue";
 import Carousel from 'primevue/carousel';
 import Image from 'primevue/image';
+import {Scenarios} from "@/data/scenarios";
 
-import { ref, nextTick, onMounted, watch } from 'vue';
+import {ref, nextTick} from 'vue';
 
 const mainCarouselKey = ref(0);
+const carouselKey = ref(Date.now());
 
-const scenarios = ref([
+const scenarios = ref(Scenarios);
+
+const responsiveOptions = ref([
   {
-    id: 1,
-    parentPhotos: [
-      {
-        id: 1,
-        title: "Antes",
-        image: "https://picsum.photos/300/200?random=1",
-        details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's stan"
-      },
-      {
-        id: 2,
-        name: "Después",
-        image: "https://picsum.photos/300/200?random=2",
-        details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem"
-      }
-    ],
-    products: [
-      { id: 1, name: "Keratina 1 id-1", brand: "Marca A", image: "https://picsum.photos/300/200?random=2" },
-      { id: 2, name: "Proteína 2 id-1", brand: "Marca B", image: "https://picsum.photos/300/200?random=3" },
-      { id: 3, name: "Creatina 3 id-1", brand: "Marca C", image: "https://picsum.photos/300/200?random=4" },
-      { id: 4, name: "Vitaminas 4 id-1", brand: "Marca D", image: "https://picsum.photos/300/200?random=5" },
-      { id: 3, name: "Creatina 5 id-1", brand: "Marca C", image: "https://picsum.photos/300/200?random=6" },
-      { id: 4, name: "Vitaminas 6 id-1", brand: "Marca D", image: "https://picsum.photos/300/200?random=7" },
-    ],
+    breakpoint: '767px',  // Para pantallas grandes
+    numVisible: 2,        // Muestra 2 imágenes
+    numScroll: 1          // Se desplaza de una en una
   },
   {
-    id: 2,
-    parentPhotos: [
-    {
-        id: 1,
-        title: "Antes",
-        image: "https://picsum.photos/300/200?random=1",
-        details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's stan"
-      },
-      {
-        id: 2,
-        name: "Después",
-        image: "https://picsum.photos/300/200?random=2",
-        details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem"
-      }
-    ],
-    products: [
-      { id: 1, name: "Keratina 1 id-2", brand: "Marca A", image: "https://picsum.photos/300/200?random=2" },
-      { id: 2, name: "Proteína 2 id-2", brand: "Marca B", image: "https://picsum.photos/300/200?random=3" },
-      { id: 3, name: "Creatina 3 id-2", brand: "Marca C", image: "https://picsum.photos/300/200?random=4" },
-      { id: 4, name: "Vitaminas 4 id-2", brand: "Marca D", image: "https://picsum.photos/300/200?random=5" },
-      { id: 3, name: "Creatina 5 id-2", brand: "Marca C", image: "https://picsum.photos/300/200?random=6" },
-      { id: 4, name: "Vitaminas 6 id-2", brand: "Marca D", image: "https://picsum.photos/300/200?random=7" },
-    ],
-  },
-  {
-    id: 3,
-    parentPhotos: [
-    {
-        id: 1,
-        title: "Antes",
-        image: "https://picsum.photos/300/200?random=1",
-        details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's stan"
-      },
-      {
-        id: 2,
-        name: "Después",
-        image: "https://picsum.photos/300/200?random=2",
-        details: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem"
-      }
-    ],
-    products: [
-      { id: 1, name: "Keratina 1 id-3", brand: "Marca A", image: "https://picsum.photos/300/200?random=2" },
-      { id: 2, name: "Proteína 2 id-3", brand: "Marca B", image: "https://picsum.photos/300/200?random=3" },
-      { id: 3, name: "Creatina 3 id-3", brand: "Marca C", image: "https://picsum.photos/300/200?random=4" },
-      { id: 4, name: "Vitaminas 4 id-3", brand: "Marca D", image: "https://picsum.photos/300/200?random=5" },
-      { id: 3, name: "Creatina 5 id-3", brand: "Marca C", image: "https://picsum.photos/300/200?random=6" },
-      { id: 4, name: "Vitaminas 6 id-3", brand: "Marca D", image: "https://picsum.photos/300/200?random=7" },
-    ],
+    breakpoint: '600px',  // Para pantallas pequeñas
+    numVisible: 1,        // Muestra solo 1 imagen
+    numScroll: 1          // Se desplaza de una en una
   }
-])
+]);
 </script>
-<style scoped>
-.carousel-container {
-  display: flex;
-  justify-content: center;
-  width: 100%;
-}
-</style>
+
+<template>
+  <div class="block lg:w-10 xl:w-8 m-auto">
+    <Carousel :value="scenarios" :key="mainCarouselKey" :numVisible="1" :numScroll="1">
+      <template #item="{ data }">
+        <div class="flex flex-auto flex-column md:flex-row flex-wrap">
+          <div v-for="parentPhoto in data.parentPhotos" :key="`scenario-${data.id}`" class="col md:col-6">
+            <div class="p-2">
+              <h2 class="text-center">{{ parentPhoto.title }}</h2>
+              <div class="mb-3">
+                <div class="relative mx-auto flex justify-content-center">
+                  <Image :src="parentPhoto.image" alt="Image" class="w-100" preview/>
+                </div>
+              </div>
+              <div class="text-center mb-3 font-medium">{{ parentPhoto.details }}</div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <Products :elements="data.products" :key="`products-${data.id}`" :scenaryId="data.id"/>
+        </div>
+      </template>
+    </Carousel>
+  </div>
+</template>
+
