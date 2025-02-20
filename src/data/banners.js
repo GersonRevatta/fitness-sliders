4243bc1dd73b918b0a5c83b330f97aba43d0a5ea
gerson <gersonrevatta@gmail.com>
2025-02-20 16:36:47 -0500

    feat: update files with images to public file

--- src/data/banners.js
+++ src/data/banners.js
@@ -1,5 +1,5 @@
 export const Banners = [
-  { src: 'src/assets/images/banner-1.jpg', alt: 'Descripción de la imagen 1' },
-  { src: 'src/assets/images/banner-2.jpg', alt: 'Descripción de la imagen 2' },
-  { src: 'src/assets/images/banner-3.jpg', alt: 'Descripción de la imagen 3' },
+  { src: './images/banner-1.jpg', alt: 'Descripción de la imagen 1' },
+  { src: './images/banner-2.jpg', alt: 'Descripción de la imagen 2' },
+  { src: './images/banner-3.jpg', alt: 'Descripción de la imagen 3' },
 ];
