9a2d7b6a0c8b3e0d942beae17ea6287cc8bd6aad
diegodev9 <diegodev9@gmail.com>
2025-02-25 11:07:48 -0300

    fix mobile

--- src/data/banners.js
+++ src/data/banners.js
@@ -1,5 +1,5 @@
 export const Banners = [
-  { src: './images/banner-1.jpg', alt: 'Descripción de la imagen 1' },
-  { src: './images/banner-2.jpg', alt: 'Descripción de la imagen 2' },
-  { src: './images/banner-3.jpg', alt: 'Descripción de la imagen 3' },
+  { src: './images/banner-1.jpg', src2: './images/banner-1-mobile.jpg' , alt: 'Descripción de la imagen 1' },
+  { src: './images/banner-2.jpg', src2: './images/banner-2-mobile.jpg' , alt: 'Descripción de la imagen 2' },
+  { src: './images/banner-3.jpg', src2: './images/banner-3-mobile.jpg' , alt: 'Descripción de la imagen 3' },
 ];
